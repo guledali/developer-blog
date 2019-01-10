adb32129f23fc89021426f01acd03d90317bec03
guledali <guled.ali01@gmail.com>
2019-01-10 16:12:02 +0100

    Add new story-knob for jumbo

--- src/stories/jumbo.stories.js
+++ src/stories/jumbo.stories.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Jumbotron, Container } from 'reactstrap'
+import { withKnobs, text } from '@storybook/addon-knobs'
 
-storiesOf(`Components`, module).add(`Jumbo`, () => (
+const stories = storiesOf('Components', module)
+stories.addDecorator(withKnobs)
+stories.add(`Jumbo`, () => (
   <Jumbotron fluid style={{ background: '#3b3c38' }}>
     <Container fluid>
       <h1 style={{ color: '#f5da55' }} className="display-3 font-weight-bold">
-        Developer blog
+        {text('Heading', 'Developer blog')}
       </h1>
-      <h2 className="display-4 text-light">Learn more about your stack</h2>
+      <h2 className="display-4 text-light">{text('Subtitle', 'Learn more about your stac')}</h2>
     </Container>
   </Jumbotron>
-))
+)).addDecorator(withKnobs)
