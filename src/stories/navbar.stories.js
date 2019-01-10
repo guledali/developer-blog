bc82de95e8684c88c63a1decb888c3a3abef820f
guledali <guled.ali01@gmail.com>
2019-01-10 15:41:21 +0100

    Added the ability update the text on navbar component

--- src/stories/navbar.stories.js
+++ src/stories/navbar.stories.js
@@ -3,15 +3,18 @@ import { storiesOf } from '@storybook/react'
 import { Navbar } from 'reactstrap'
 import { Link } from 'gatsby'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { withKnobs, text } from '@storybook/addon-knobs'
 
-storiesOf(`Components`, module).add(`NavbarDark`, () => (
+const stories = storiesOf('Components', module)
+stories.addDecorator(withKnobs)
+stories.add(`NavbarDark`, () => (
   <Navbar style={{ background: '#323330' }} className="shadow-sm">
     <Link
       to="/"
       style={{ color: '#f5da55', textDecoration: 'none' }}
       className="navbar-brand"
     >
-      Developer blog
+      {text('Text', 'Developer blog')}
     </Link>
   </Navbar>
-))
+)).addDecorator(withKnobs)
