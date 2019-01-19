8f360ad2783a29d3f2b3aa20566adc9fe79f4bd5
guledali <guled.ali01@gmail.com>
2019-01-19 19:32:18 +0100

    adding correct stuff

--- src/utils/formatDate.test.js
+++ src/utils/formatDate.test.js
@@ -3,5 +3,5 @@ import formatDate from './formatDate'
 test('formatDate works', () => {
   const value = formatDate('2018-12-21T14:07:09.256Z')
 
-  expect(value).toBe('2018-12-22')
+  expect(value).toBe('2018-12-21')
 })
